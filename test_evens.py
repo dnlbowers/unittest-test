f4e1c5638bd342b48908b9a1bebdcbd2d869d23a
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-01-20 09:31:23 +0100

    Create firtst tests with unit test

--- test_evens.py
+++ test_evens.py
@@ -1,10 +1,18 @@
+from typing import Type
 import unittest
 from evens import even_number_of_evens  # import function
 
 
 class TestEvens(unittest.TestCase):
 
-    pass
+    def test_throw_error_if_value_passed_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+    def test_value_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
 
 
 if __name__ == '__main__':
